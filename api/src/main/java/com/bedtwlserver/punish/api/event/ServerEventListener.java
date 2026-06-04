0b91a523b02f72af7dc099784db083498b26101d
ItsGlobally <itsglobally1324@gmail.com>
2026-06-04 18:27:40 +0800

    Add ServerEventListener interface for handling cross-server events

---
+++ api/src/main/java/com/bedtwlserver/punish/api/event/ServerEventListener.java
@@ -0,0 +1,20 @@
+package com.bedtwlserver.punish.api.event;
+
+/**
+ * 伺服器事件監聽器
+ * 用於監聽和處理來自其他伺服器的事件
+ */
+public interface ServerEventListener {
+    
+    /**
+     * 處理事件
+     * @param event 接收到的事件
+     */
+    void onEvent(ServerEvent event);
+    
+    /**
+     * 獲取該監聽器支持的事件類型
+     * @return 支持的事件類型（如 "ban", "mute" 等），或 "*" 表示監聽所有事件
+     */
+    String getEventType();
+}
